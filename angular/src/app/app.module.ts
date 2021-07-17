ed49283e469ec4aad4987c1ec7c4890d49c4a943
keerthanarr <86659882+keerthanarr@users.noreply.github.com>
2021-07-18 00:02:15 +0530

    routing added

---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CompinComponent } from './compin/compin.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CompinComponent,
+    ParentComponent,
+    ChildComponent,
+    HomeComponent,
+    LoginComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular/src/app/compin/compin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-compin',
+  templateUrl: './compin.component.html',
+  styleUrls: ['./compin.component.css']
+})
+export class CompinComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
